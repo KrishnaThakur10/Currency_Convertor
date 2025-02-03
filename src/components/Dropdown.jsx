3132f4f65526419ed854f27e699270d6087cb879
KrishnaThakur10 <krishnathakur27082005@gmail.com>
2025-02-03 19:37:28 +0530

    update

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiOutlineStar } from "react-icons/hi2";
+import { HiMiniStar } from "react-icons/hi2";
 const CurrencyDropdown = ({
     currencies,
     currency, 
@@ -14,7 +15,7 @@ const CurrencyDropdown = ({
     <div>
       <label htmlFor={title} className='text-sm block font-medium text-gray-700'>{title}</label>
       <div className='relative'>
-        <select value={currency} onChange={(e)=>setcurrency(e.target.value)} className='w-full p-1 m-1  rounded-md border-gray-300 shadow-sm bg-gray-200 focus:outline-none focus:ring-2 focus:ring-indigo-500'>
+        <select value={currency} onChange={(e)=>setcurrency(e.target.value)} className='w-full p-1 m-1  rounded-md border-gray-300 shadow-sm   focus:outline-none focus:ring-2 focus:ring-indigo-500'>
             {/* {favorites.map((currency)=>{
                 return(
                   <option className='bg-gray-200' value={currency} key={currency}>
@@ -33,15 +34,17 @@ const CurrencyDropdown = ({
 
             {uniqueCurrencies?.map((currency)=>{
                 return(
-                  <option  value={currency} key={currency}>
+                  <option  value={currency} key={currency} className={favorites.includes(currency) ? 'bg-gray-100': 'bg-white'}>
                     {currency}
                   </option>
                   )
             })} 
         </select>
-        <button onClick={()=>handlefavourite(currency)} className='absolute right-0 inset-y-0 pr-3 flex items-center leading-5 text-sm'>
-        <HiOutlineStar />
+        {uniqueCurrencies?.map((currency)=>{
+        <button key={currency} onClick={()=>handlefavourite(currency)} className='absolute right-0 inset-y-0 pr-3 flex items-center leading-5 text-sm'>
+          {favorites.includes(currency) ? <HiMiniStar /> : <HiOutlineStar />}    
         </button>
+            })} 
       </div>
     </div>
   )
